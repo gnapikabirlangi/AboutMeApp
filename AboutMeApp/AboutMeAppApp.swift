43cc54f0ff78053e98fe30629609560ceb0564c6
Gnapika Birlangi <gnapib5@gmail.com>
2023-07-28 10:23:11 -0700

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by Gnapika Birlangi on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMeApp/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  AboutMeApp
+//
+//  Created by Gnapika Birlangi on 7/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var textTitle = ""
+    var body: some View {
+        ZStack {
+            Color(red: 0.7, green: 0.7, blue: 5)
+                .ignoresSafeArea()
+            VStack {
+                Text("Gnapika")
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                Text("About Me")
+                Image("rollercoaster")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Button("Click Here"){
+                    textTitle = "- Love rollercoasters but hate heights"
+                }
+                .font(.title2)
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+                Text(textTitle)
+            }
+            .padding()
+            .background(.white)
+            .cornerRadius(20)
+            .padding()
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
